97c890dda7563637700c1c29a60a48d7b2362f26
Evgeny <intercept_pirat@mail.ru>
2016-12-13 15:10:13 +0200

    Update rename.sh

--- compose/rename.sh
+++ compose/rename.sh
@@ -2,8 +2,8 @@
 if [ -z $1 ];
   then echo "Do not set new name";
   else
-    sed -i -e "s/my_project/$1/g" docker-compose.yaml;
-    sed -i -e "s/php:9000/$1\_php:9000/g" nginx/etc/conf.d/default.conf;
-    sed -i -e "s/my_project/$1/g" artisan.sh;
-    sed -i -e "s/my_project/$1/g" composer.sh;
+    sed -i -e "s/project/$1/g" docker-compose.yaml;
+    sed -i -e "s/project:9000/$1\_php:9000/g" nginx/etc/conf.d/default.conf;
+    sed -i -e "s/project/$1/g" artisan.sh;
+    sed -i -e "s/project/$1/g" composer.sh;
 fi
