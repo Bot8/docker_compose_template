c01534257e6052666a60ec60dca0312ae47573e3
artarn <artarn@yandex.ru>
2017-01-25 09:58:47 +0300

    bugfix

--- docker/rename.sh
+++ docker/rename.sh
@@ -3,7 +3,7 @@ if [ -z $1 ];
   then echo "Do not set new name";
   else
     sed -i -e "s/project/$1/g" docker-compose.yaml;
-    sed -i -e "s/project:9000/$1\_php:9000/g" nginx/etc/conf.d/default.conf;
+    sed -i -e "s/project/$1/g" nginx/etc/conf.d/default.conf;
     sed -i -e "s/project/$1/g" artisan.sh;
     sed -i -e "s/project/$1/g" composer.sh;
 fi
